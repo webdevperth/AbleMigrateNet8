75f48b8e43001531ca9518ca8dd069fc3eb8972d
Andrew Hollander <webdevperth@gmail.com>
2026-05-25 00:19:42 +0800

    [migration-continued] Migration Step 12 — Integration cleanup and final verification. Fixed layout heaer & sidebar was loading as razor partial instead of view component.

--- Integral.Web.AlbertSite/AppCode/PageBaseClasses/LoggedInPageModel.cs
+++ Integral.Web.AlbertSite/AppCode/PageBaseClasses/LoggedInPageModel.cs
@@ -1,7 +1,6 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
 using System.Threading.Tasks;
 
 namespace Integral.Web.PortalSite.AppCode.PageBaseClasses {
